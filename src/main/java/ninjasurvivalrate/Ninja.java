720dab3ac426198f4bef7c83898620e65d87c40a
Abel Cuenca <abel.cuenca@gmail.com>
2011-07-17 20:30:20 +0200

    cosmmetic changes

--- src/main/java/ninjasurvivalrate/Ninja.java
+++ src/main/java/ninjasurvivalrate/Ninja.java
@@ -1,16 +1,21 @@
 package ninjasurvivalrate;
+
 import java.util.Arrays;
 import java.util.Collection;
-public class Ninja {
-private String belt;
-public Ninja (String belt){
-this.belt=belt;
-}
-public Collection<String> attackedBy(String opponent){
-if ("Chuck Norris".equals(opponent))
-return Arrays.asList(new String[]{"run for his life"});
-else
-return Arrays.asList(new String[]{"engage the opponent"});
-}
-}
 
+public class Ninja {
+
+    private String belt;
+
+    public Ninja(String belt) {
+        this.belt = belt;
+    }
+
+    public Collection<String> attackedBy(String opponent) {
+        if ("Chuck Norris".equals(opponent)) {
+            return Arrays.asList(new String[]{"run for his life"});
+        } else {
+            return Arrays.asList(new String[]{"engage the opponent"});
+        }
+    }
+}
